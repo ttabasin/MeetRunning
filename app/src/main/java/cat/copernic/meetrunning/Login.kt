47dad1969bf853aea81c35ddfd57c354d576e163
Santiago Alves <alves.pizzorno.santiago.nahuel@alumnat.copernic.cat>
2021-10-28 17:17:48 +0200

    Fix Sign Up button

--- app/src/main/java/cat/copernic/meetrunning/Login.kt
+++ app/src/main/java/cat/copernic/meetrunning/Login.kt
@@ -9,10 +9,10 @@ class Login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
-        /*val signUpBt =findViewById<Button>(R.id.signUpBt)
+        val signUpBt =findViewById<Button>(R.id.signUpBt)
         signUpBt.setOnClickListener {
             val intent = Intent(this, SignUp::class.java)
             startActivity(intent)
-        }*/
+        }
     }
 }
