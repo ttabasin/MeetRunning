b53e8318e17d4d6fe501ac6449346caf0a1a58ee
Tabasin <cpau200@gmail.com>
2021-11-02 18:49:38 +0100

    Pantall SignUp

--- app/src/main/java/cat/copernic/meetrunning/SignUpActivity.kt
+++ app/src/main/java/cat/copernic/meetrunning/SignUpActivity.kt
@@ -1,11 +1,26 @@
 package cat.copernic.meetrunning
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import cat.copernic.meetrunning.databinding.ActivitySignUpBinding
 
 class SignUpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+        val binding = DataBindingUtil.setContentView<ActivitySignUpBinding>(this, R.layout.activity_sign_up)
+        binding.signUpContinue.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+        binding.signInBtn.setOnClickListener {
+            val intent = Intent(this, SignInActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
