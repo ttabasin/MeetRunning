d3daee71593d6d5ad2539e2e2e783a26e10b4ffb
ttabasin <cpau200@gmail.com>
2021-11-05 09:22:20 +0100

    addRoute check

--- app/src/main/java/cat/copernic/meetrunning/AddRouteFragment.kt
+++ app/src/main/java/cat/copernic/meetrunning/AddRouteFragment.kt
@@ -5,20 +5,34 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.findNavController
+import cat.copernic.meetrunning.databinding.FragmentAddRouteBinding
+import cat.copernic.meetrunning.databinding.FragmentHomeBinding
 
 class AddRouteFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
-
+    private lateinit var binding: FragmentAddRouteBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+        binding = FragmentAddRouteBinding.inflate(layoutInflater)
+
+        binding.btnContinue.setOnClickListener {
+            if (checkInput()){
+                it.findNavController().navigate(R.id.action_addRoute_to_addRouteMap)
+            }
+        }
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_route, container, false)
+        return binding.root
+    }
+
+    private fun checkInput(): Boolean {
+        if (binding.editTitle.text.isNotBlank() && binding.editDescription.text.isNotBlank()) {
+            return true
+        }
+        return false
     }
 
 }
