faf818c000b3608175deafad39891a8f24db33f7
Santiago Alves <alves.pizzorno.santiago.nahuel@alumnat.copernic.cat>
2021-11-05 13:27:45 +0100

    fer el recyclerView a la pantalla home

--- app/src/main/java/cat/copernic/meetrunning/HomeFragment.kt
+++ app/src/main/java/cat/copernic/meetrunning/HomeFragment.kt
@@ -1,6 +1,7 @@
 package cat.copernic.meetrunning
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -8,21 +9,19 @@ import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil.setContentView
 import androidx.navigation.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import cat.copernic.meetrunning.R.layout.fragment_home
 import cat.copernic.meetrunning.databinding.FragmentHomeBinding
+import com.google.firebase.database.*
+import com.google.firebase.firestore.FirebaseFirestore
 
 
 class HomeFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        val binding = FragmentHomeBinding.inflate(layoutInflater)
-
-        //binding.recycler.layoutManager = LinearLayoutManager(this)
-
-    }
-
+    private val db = FirebaseFirestore.getInstance()
+    private lateinit var dbref: DatabaseReference
+    private lateinit var postRecyclerView: RecyclerView
+    private lateinit var postArrayList: ArrayList<Post>
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -34,9 +33,39 @@ class HomeFragment : Fragment() {
         binding.floatingActionButton.setOnClickListener {
             it.findNavController().navigate(R.id.action_home_to_addRoute)
         }
+
+
+        postRecyclerView = binding.recycler
+        postRecyclerView.layoutManager = LinearLayoutManager(context)
+        postRecyclerView.setHasFixedSize(true)
+
+        postArrayList = arrayListOf<Post>()
+        getPostData()
+
         // Inflate the layout for this fragment
         return binding.root
     }
 
+    private fun getPostData() {
+        dbref = FirebaseDatabase.getInstance().getReference("Posts")
+        dbref.addValueEventListener(object : ValueEventListener {
+
+            override fun onDataChange(snapshot: DataSnapshot) {
+
+                if (snapshot.exists()) {
+
+                    for (postSnapshot in snapshot.children) {
+                        val post = postSnapshot.getValue(Post::class.java)
+                        postArrayList.add(post!!)
+                    }
+                    postRecyclerView.adapter = PostAdapter(postArrayList)
+                }
+            }
+
+            override fun onCancelled(p0: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+        })
+    }
 
 }
---
+++ app/src/main/java/cat/copernic/meetrunning/Post.kt
@@ -0,0 +1,7 @@
+package cat.copernic.meetrunning
+
+import android.widget.ImageView
+
+data class Post(var title : String ?= null,
+                var description : String ?= null/*,
+                var image: ImageView ?= null*/)
