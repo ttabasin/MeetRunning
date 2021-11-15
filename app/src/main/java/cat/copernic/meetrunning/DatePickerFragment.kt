1b3249602aafe305bb7fe336e583949bf7496c80
Santiago Alves <alves.pizzorno.santiago.nahuel@alumnat.copernic.cat>
2021-11-15 12:52:44 +0100

    Fer calendari pantalla ranking

---
+++ app/src/main/java/cat/copernic/meetrunning/DatePickerFragment.kt
@@ -0,0 +1,44 @@
+package cat.copernic.meetrunning
+
+import android.app.DatePickerDialog
+import android.app.Dialog
+import android.content.Context
+import android.os.Bundle
+import android.util.Log
+import android.widget.DatePicker
+import androidx.fragment.app.DialogFragment
+import androidx.fragment.app.setFragmentResult
+import java.text.SimpleDateFormat
+import java.util.*
+
+class DatePickerFragment :
+    DialogFragment(), DatePickerDialog.OnDateSetListener {
+
+    private val c = Calendar.getInstance()
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val year = c.get(Calendar.YEAR)
+        val month = c.get(Calendar.MONTH)
+        val day = c.get(Calendar.DAY_OF_MONTH)
+
+        return DatePickerDialog(requireActivity(), this, year, month, day)
+    }
+
+    override fun onDateSet(view: DatePicker?, year: Int, month: Int, day: Int) {
+        c.set(Calendar.YEAR, year)
+        c.set(Calendar.MONTH, month)
+        c.set(Calendar.DAY_OF_MONTH, day)
+
+        val selectedDate = SimpleDateFormat("MM", Locale.ENGLISH).format(c.time)
+
+        val selectedDateBundle = Bundle()
+
+        selectedDateBundle.putString("SELECTED_DATE", selectedDate)
+
+        setFragmentResult("REQUEST_KEY", selectedDateBundle)
+
+    }
+
+
+
+}
--- app/src/main/java/cat/copernic/meetrunning/RankingFragment.kt
+++ app/src/main/java/cat/copernic/meetrunning/RankingFragment.kt
@@ -2,23 +2,94 @@ package cat.copernic.meetrunning
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import cat.copernic.meetrunning.databinding.FragmentRankingBinding
+import java.util.*
 
-class RankingFragment : Fragment() {
+class RankingFragment : Fragment(R.layout.fragment_ranking) {
+    private var _binding: FragmentRankingBinding? = null
+    private val binding get() = _binding!!
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        _binding = FragmentRankingBinding.bind(view)
 
+        binding.apply {
+
+            //Posar el mes actual com a text
+
+            val c = Calendar.getInstance()
+            val currentMonth = c.get(Calendar.MONTH) + 1
+            monthText.text = changeMonth(currentMonth.toString())
+
+
+            calendar.setOnClickListener {
+                //Crear una instància de DatePickerFragment
+                val datePickerFragment = DatePickerFragment()
+                val supportFragmentManager = requireActivity().supportFragmentManager
+
+                supportFragmentManager.setFragmentResultListener(
+                    "REQUEST_KEY",
+                    viewLifecycleOwner
+                ) { resultKey, bundle ->
+                    //Canviar el text del mes
+                    if (resultKey == "REQUEST_KEY") {
+                        val date = bundle.getString("SELECTED_DATE")
+                        monthText.text = changeMonth(date.toString())
+                    }
+                }
+
+                //Mostrar el calendari
+                datePickerFragment.show(supportFragmentManager, "DatePickerFragment")
+            }
+        }
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_ranking, container, false)
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
+    private fun changeMonth(date: String): String {
+        lateinit var month: String
+        when {
+            date.toString() == "01" -> {
+                month = "Gener"
+            }
+            date.toString() == "02" -> {
+                month = "Febrer"
+            }
+            date.toString() == "03" -> {
+                month = "Març"
+            }
+            date.toString() == "04" -> {
+                month = "Abril"
+            }
+            date.toString() == "05" -> {
+                month = "Maig"
+            }
+            date.toString() == "06" -> {
+                month = "Juny"
+            }
+            date.toString() == "07" -> {
+                month = "Juliol"
+            }
+            date.toString() == "08" -> {
+                month = "Agost"
+            }
+            date.toString() == "09" -> {
+                month = "Setembre"
+            }
+            date.toString() == "10" -> {
+                month = "Octubre"
+            }
+            date.toString() == "11" -> {
+                month = "Novembre"
+            }
+            date.toString() == "12" -> {
+                month = "Desembre"
+            }
+        }
+        return month
+    }
 }
