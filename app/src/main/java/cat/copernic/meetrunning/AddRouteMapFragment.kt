bfa519946be0b81220d06d7e4c602b97404ef57d
ttabasin <cpau200@gmail.com>
2021-11-15 12:53:28 +0100

    Map ubicacion

--- app/src/main/java/cat/copernic/meetrunning/AddRouteMapFragment.kt
+++ app/src/main/java/cat/copernic/meetrunning/AddRouteMapFragment.kt
@@ -1,11 +1,17 @@
 package cat.copernic.meetrunning
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import cat.copernic.meetrunning.databinding.FragmentAddRouteMapBinding
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -14,10 +20,13 @@ import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 
 
+
 class AddRouteMapFragment : Fragment(), OnMapReadyCallback {
 
     private lateinit var binding: FragmentAddRouteMapBinding
     private lateinit var mMap: GoogleMap
+    private val REQUEST_LOCATION_PERMISSION = 1
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -35,13 +44,65 @@ class AddRouteMapFragment : Fragment(), OnMapReadyCallback {
     override fun onMapReady(p0: GoogleMap) {
         mMap = p0
         mMap.mapType = GoogleMap.MAP_TYPE_NORMAL
-        //mMap.isMyLocationEnabled = true
+        enableMyLocation()
         val sydney = LatLng(-34.0, 151.0)
         mMap.addMarker(
             MarkerOptions()
             .position(sydney)
             .title("Marker in Sydney"))
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun enableMyLocation(){
+        if (isPermissionGranted()){
+            mMap.isMyLocationEnabled = true
+        }else {
+            ActivityCompat.requestPermissions(
+                requireActivity(),
+                arrayOf<String>(Manifest.permission.ACCESS_FINE_LOCATION),
+                REQUEST_LOCATION_PERMISSION
+            )
+        }
+    }
+
+    private fun isPermissionGranted() : Boolean {
+        return ContextCompat.checkSelfPermission(
+            requireContext(),
+            Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray) {
+        // Check if location permissions are granted and if so enable the
+        // location data layer.
+        if (requestCode == REQUEST_LOCATION_PERMISSION) {
+            if (grantResults.contains(PackageManager.PERMISSION_GRANTED)) {
+                enableMyLocation()
+            }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        binding.mapView.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        binding.mapView.onPause()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding.mapView.onDestroy()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        binding.mapView.onLowMemory()
     }
 
 }
