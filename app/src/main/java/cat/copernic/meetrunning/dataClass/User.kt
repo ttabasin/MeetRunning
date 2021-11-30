b3924e534dbc4f36a466c5beaa95182df27ffe2d
Santiago Alves <alves.pizzorno.santiago.nahuel@alumnat.copernic.cat>
2021-11-30 13:46:51 +0100

    viewModel i xml Settings

--- app/src/main/java/cat/copernic/meetrunning/dataClass/User.kt
+++ app/src/main/java/cat/copernic/meetrunning/dataClass/User.kt
@@ -1,5 +1,7 @@
 package cat.copernic.meetrunning.dataClass
 
 data class User(var username : String ?= null,
-                var distance : Int ?= null/*,
+                var distance : Int ?= null,
+                var time: Long? = 0,
+                var description: String? = ""/*,
                 var image: ImageView ?= null*/)
