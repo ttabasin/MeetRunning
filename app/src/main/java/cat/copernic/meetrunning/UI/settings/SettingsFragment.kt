c36a18bfbff77966d06d85b11693173423fdd898
Santiago Alves <alves.pizzorno.santiago.nahuel@alumnat.copernic.cat>
2021-12-03 08:13:04 +0100

    fix settingsFragment

--- app/src/main/java/cat/copernic/meetrunning/UI/settings/SettingsFragment.kt
+++ app/src/main/java/cat/copernic/meetrunning/UI/settings/SettingsFragment.kt
@@ -1,21 +1,23 @@
 package cat.copernic.meetrunning.UI.settings
 
+import android.content.Intent
 import android.content.res.Configuration
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.RadioButton
 import android.widget.Toast
-import androidx.databinding.DataBindingUtil.setContentView
-import cat.copernic.meetrunning.R
+import androidx.annotation.RequiresApi
+import cat.copernic.meetrunning.MainActivity
 import cat.copernic.meetrunning.databinding.FragmentSettingsBinding
 import java.util.*
 
 class SettingsFragment : Fragment() {
 
+    @RequiresApi(Build.VERSION_CODES.N)
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -23,12 +25,23 @@ class SettingsFragment : Fragment() {
 
         var binding = FragmentSettingsBinding.inflate(layoutInflater)
 
+        Log.i("settings", "${context!!.resources.configuration.locales}")
+
+        if (context!!.resources.configuration.locales.toString() == "[en_US]") {
+            binding.rbEnglish.isChecked = true
+        } else if (context!!.resources.configuration.locales.toString() == "[es_ES]") {
+            binding.rbSpanish.isChecked = true
+        } else if (context!!.resources.configuration.locales.toString() == "[ca_ES]") {
+            binding.rbCat.isChecked = true
+        }
+
         binding.radioGroup.setOnCheckedChangeListener { group, checkedId ->
 
-            if(checkedId == binding.rbEnglish.id){
-                Toast.makeText(context, binding.rbEnglish.text.toString(), Toast.LENGTH_SHORT).show()
+            if (checkedId == binding.rbEnglish.id) {
+                Toast.makeText(context, binding.rbEnglish.text.toString(), Toast.LENGTH_SHORT)
+                    .show()
+
                 val locale = Locale("en", "EN")
-                Locale.setDefault(locale)
                 val config: Configuration = context!!.resources.configuration
                 config.setLocale(locale)
 
@@ -38,10 +51,9 @@ class SettingsFragment : Fragment() {
                 )
 
             }
-            if(checkedId == binding.rbSpanish.id){
+            if (checkedId == binding.rbSpanish.id) {
                 Toast.makeText(context, binding.rbSpanish.text.toString(), Toast.LENGTH_SHORT).show()
                 val locale = Locale("es", "ES")
-                Locale.setDefault(locale)
                 val config: Configuration = context!!.resources.configuration
                 config.setLocale(locale)
 
@@ -49,11 +61,26 @@ class SettingsFragment : Fragment() {
                     config,
                     context!!.resources.displayMetrics
                 )
+
             }
 
-            if(checkedId == binding.rbCat.id){
+            if (checkedId == binding.rbCat.id) {
                 Toast.makeText(context, binding.rbCat.text.toString(), Toast.LENGTH_SHORT).show()
+                val locale = Locale("ca", "ES")
+                val config: Configuration = context!!.resources.configuration
+                config.setLocale(locale)
+
+                context!!.resources.updateConfiguration(
+                    config,
+                    context!!.resources.displayMetrics
+                )
+
             }
+
+            val intent = Intent(context, MainActivity::class.java)
+            startActivity(intent)
+            activity?.finish()
+
         }
 
         return binding.root
