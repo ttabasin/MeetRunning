7d0149e1290164cf537f2e9663f5de5573f543cb
Santiago Alves <alves.pizzorno.santiago.nahuel@alumnat.copernic.cat>
2021-12-10 09:55:52 +0100

    Achievements complete

--- app/src/main/java/cat/copernic/meetrunning/UI/profile/AchivementsFragment.kt
+++ app/src/main/java/cat/copernic/meetrunning/UI/profile/AchivementsFragment.kt
@@ -6,12 +6,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.core.view.isVisible
 import androidx.navigation.findNavController
 import cat.copernic.meetrunning.R
 import cat.copernic.meetrunning.databinding.FragmentAchivementsBinding
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
+import java.text.SimpleDateFormat
+import java.util.*
 
 open class AchivementsFragment : Fragment() {
 
@@ -26,6 +29,8 @@ open class AchivementsFragment : Fragment() {
         var binding = FragmentAchivementsBinding.inflate(layoutInflater)
         val currentUserEmail = FirebaseAuth.getInstance().currentUser?.email.toString()
         db = FirebaseFirestore.getInstance()
+        var countDist = 0
+        var countTime = 0
 
         binding.myRoutesBT.setOnClickListener{
             it.findNavController().navigate(AchivementsFragmentDirections.actionAchivementsToMyRoutes())
@@ -50,23 +55,23 @@ open class AchivementsFragment : Fragment() {
                 binding.achDist1.isVisible = true
                 binding.achDist2.isVisible = true
                 binding.achDist3.isVisible = true
-                binding.achDistT1.isVisible = false
-                binding.achDistT2.isVisible = false
-                binding.achDistT3.isVisible = false
+                binding.achTime1.isVisible = false
+                binding.achTime2.isVisible = false
+                binding.achTime3.isVisible = false
             }
 
         }
 
         binding.achTime.setOnClickListener{
-            if(binding.achDistT1.isVisible){
-                binding.achDistT1.isVisible = false
-                binding.achDistT2.isVisible = false
-                binding.achDistT3.isVisible = false
+            if(binding.achTime1.isVisible){
+                binding.achTime1.isVisible = false
+                binding.achTime2.isVisible = false
+                binding.achTime3.isVisible = false
             }else{
 
-                binding.achDistT1.isVisible = true
-                binding.achDistT2.isVisible = true
-                binding.achDistT3.isVisible = true
+                binding.achTime1.isVisible = true
+                binding.achTime2.isVisible = true
+                binding.achTime3.isVisible = true
 
                 binding.achDist1.isVisible = false
                 binding.achDist2.isVisible = false
@@ -78,8 +83,55 @@ open class AchivementsFragment : Fragment() {
 
         db.collection("users").document(currentUserEmail).get().addOnSuccessListener {
             binding.description.text = it.getString("description")
-        }
 
+            //Distance
+            if (it.getDouble("distance")!! >= 25) {
+                binding.achDist1.setBackgroundColor(ContextCompat.getColor(context!!, R.color.appBar))
+                countDist++
+            }else{
+                binding.achDist1.setCardBackgroundColor(ContextCompat.getColor(context!!, R.color.grey))
+            }
+
+            if (it.getDouble("distance")!! >= 50) {
+                binding.achDist2.setCardBackgroundColor(ContextCompat.getColor(context!!, R.color.appBar))
+                countDist++
+            }else{
+                binding.achDist2.setCardBackgroundColor(ContextCompat.getColor(context!!, R.color.grey))
+            }
+
+            if (it.getDouble("distance")!! >= 100) {
+                countDist++
+                binding.achDist3.setCardBackgroundColor(ContextCompat.getColor(context!!, R.color.appBar))
+            }else{
+                binding.achDist3.setCardBackgroundColor(ContextCompat.getColor(context!!, R.color.grey))
+            }
+
+            binding.achCompleteNumD.text = countDist.toString()
+
+            //Time
+            if (SimpleDateFormat("HH:mm:ss").format(it.get("time").toString().toInt().minus(TimeZone.getDefault().rawOffset))!! >= "01:00:00") {
+                binding.achTime1.setBackgroundColor(ContextCompat.getColor(context!!, R.color.appBar))
+                countTime++
+            }else{
+                binding.achTime1.setCardBackgroundColor(ContextCompat.getColor(context!!, R.color.grey))
+            }
+
+            if (SimpleDateFormat("HH:mm:ss").format(it.get("time").toString().toInt().minus(TimeZone.getDefault().rawOffset))!! >= "05:00:00") {
+                binding.achTime2.setCardBackgroundColor(ContextCompat.getColor(context!!, R.color.appBar))
+                countTime++
+            }else{
+                binding.achTime2.setCardBackgroundColor(ContextCompat.getColor(context!!, R.color.grey))
+            }
+
+            if (SimpleDateFormat("HH:mm:ss").format(it.get("time").toString().toInt().minus(TimeZone.getDefault().rawOffset))!! >= "10:00:00") {
+                binding.achTime3.setCardBackgroundColor(ContextCompat.getColor(context!!, R.color.appBar))
+                countTime++
+            }else{
+                binding.achTime3.setCardBackgroundColor(ContextCompat.getColor(context!!, R.color.grey))
+            }
+            binding.achCompleteNumT.text = countTime.toString()
+
+        }
 
 
 
@@ -87,3 +139,4 @@ open class AchivementsFragment : Fragment() {
     }
 
 }
+
